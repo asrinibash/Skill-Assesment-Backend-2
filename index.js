e1b2f37203b53895c890662328cf583b49e2d52f
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-07-19 13:23:33 +0530

    Updated index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const notificationRoutes = require('./routes/NotificationRoutes');
+const makeDatabaseConnection = require('./config/DatabaseConnection');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use('/api/notifications', notificationRoutes);
+
+const PORT = process.env.PORT || 8000;
+
+async function startServer() {
+  try {
+    await makeDatabaseConnection();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error starting server:', error);
+  }
+}
+
+startServer();
